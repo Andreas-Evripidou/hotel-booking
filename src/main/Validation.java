acbf4bf9b6d131f9e486ff3524ac5a32d62bb993
Andreas Matoli <amatoli1@sheffield.ac.uk>
2021-11-10 22:08:20 +0000

    Added the validation method needed by the login page. Also I implemented some methods for adding a new user to the database and checking if already excist

--- src/main/Validation.java
+++ src/main/Validation.java
@@ -1,9 +1,61 @@
 package main;
 
+import java.sql.*;
+
 public class Validation {
 	
-//	public boolean validateUser(String userID, String password) {
-//		return True;
-//	}
-
+	public boolean validateUser(String userID, String password, String whatUser) {
+		if (whatUser != null) {
+			String query = "SELECT * FROM team023." + whatUser + " WHERE userID='" + userID + "'";
+			DatabaseCommunication db = new DatabaseCommunication();
+			String user = null;
+			String pass = null;
+			try {
+				ResultSet results = db.queryExecute(query);
+				while (results.next()) {
+					user = results.getString(1);
+					pass = results.getString(2);
+				}
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
+				}
+			return user != null && pass != null && user.equals(userID) && pass.equals(password);
+		}
+		return false;
+	}
+	
+	public void validateUserRegistration(Person user, Address userAddress) {
+		String insertAddress = "INSERT INTO `team023`.`Address` (`house`, `postcode`, `streetName`, `placeName`) VALUES(?, ?, ?, ?);";
+		String insertPerson = "INSERT INTO `team023`.`Person` (`userID`, `password`, `title`, `name`, `surname`, `contactDetails`, `host`, `guest`, `house`, `postcode`) "
+				+ "VALUES(?, ?, ?, ?, ?, ?, ?, ?, ?, ?);";
+		DatabaseCommunication db = new DatabaseCommunication();
+		
+		try {
+			db.addUserAddressInDatabase(userAddress, insertAddress);
+			db.addUserInDatabase(user, userAddress, insertPerson);
+		} catch (Exception e) {
+			e.printStackTrace();
+		} finally {
+			db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
+		}
+	}
+	
+	public boolean alreadyExcists(String userID) {
+		String query = "SELECT COUNT(*) FROM team023.Person WHERE userID='" + userID + "'";
+		DatabaseCommunication db = new DatabaseCommunication();
+		int n = 0;
+		try {
+			ResultSet result = db.queryExecute(query);
+			while (result.next()) {
+				n = result.getInt(1);
+			}
+			} catch (Exception e) {
+				e.printStackTrace();
+			} finally {
+				db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
+			}
+		return n > 0;
+	}
 }
