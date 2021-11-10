62a19edfdf6ba78bbc606ac76c4a0274cd199c48
Andreas Matoli <amatoli1@sheffield.ac.uk>
2021-11-10 22:06:37 +0000

    added connectors for the database

--- src/main/DatabaseCommunication.java
+++ src/main/DatabaseCommunication.java
@@ -10,48 +10,59 @@ public class DatabaseCommunication {
 	
 	private Connection con = null;
 	private Statement stmt = null;
+	private PreparedStatement pstmt = null;
 	private ResultSet res = null;
 	
 
 	public static void main(String [] args) {
-//		try (Connection con = DriverManager.getConnection( "jdbc:mysql://stusql.dcs.shef.ac.uk/team023", "team023", "64783854")) {
-//			// use the open connection // for one or more queries
-//			try (Statement stmt = con.createStatement()) {
-//				
-//				ResultSet res = stmt.executeQuery("SELECT * FROM team023.Guest;");
-//				
-//				System.out.println(res);
-//				
-//				while (res.next()) {
-//					String userID = res.getString(1);
-//					System.out.println(userID);
-//				}
-//				
-//			}
-//			catch (SQLException ex) {
-//				ex.printStackTrace();
-//			}
-//		}
-//		catch (Exception ex) {
-//			ex.printStackTrace();
-//		}
 		DatabaseCommunication db = new DatabaseCommunication();
-		try {
-			ResultSet results = db.openConnectionForQuery("SELECT * FROM team023.Guest;");
-			while (results.next()) {
-				String userID = results.getString(1);
-				System.out.println(userID);
-			}
-		} catch (Exception e) {
-			e.printStackTrace();
+//		try {
+//			ResultSet results = db.openConnectionForQuery("SELECT * FROM team023.Guest;");
+//			while (results.next()) {
+//				String userID = results.getString(1);
+//				System.out.println(userID);
+//			}
+//		} catch (Exception e) {
+//			e.printStackTrace();
+//		}
+//		finally {
+//			db.closeAll(db.res, db.stmt, db.pstmt, db.con);
+//		}
+		
+		Validation v = new Validation();
+//		System.out.println(v.validateUser("malakas@email.com", "123456", "Host"));
+//		db.closeAll(db.res, db.stmt, db.pstmt, db.con);
+		
+		Person m = new Person("am", "am", "am", "am", 12343, 1, 0, "am");
+		Address a = new Address("am", "am", "am", "am");
+		
+		if (!v.alreadyExcists(m.getEmail())) {
+			v.validateUserRegistration(m, a);
+			System.out.println("added user");
+		} else {
+			System.out.println("already excists");
 		}
-		finally {
-			db.closeAll(db.res, db.stmt, db.con);
-		}
+		
 		
 	}
 	
-	public ResultSet openConnectionForQuery(String query) throws SQLException, Exception {
+	public Connection getConnection() {
+		return con;
+	}
+	
+	public Statement getStatement() {
+		return stmt;
+	}
+	
+	public PreparedStatement getPreparedStatement() {
+		return pstmt;
+	}
+	
+	public ResultSet getResultSet() {
+		return res;
+	}
+	
+	public ResultSet queryExecute(String query) throws SQLException, Exception {
 		
 //		Connection con = null;
 		
@@ -75,7 +86,80 @@ public class DatabaseCommunication {
 		return null;
 	}
 	
-	public void closeAll(ResultSet res, Statement stmt, Connection con) {
+	public void addUserInDatabase(Person p, Address a, String statement) throws SQLException, Exception {
+		try {
+			con = DriverManager.getConnection(SERVER, DBUSER, PASSWORD);
+			try {
+				pstmt = con.prepareStatement(statement);
+				
+				pstmt.setString(1, p.getEmail());
+				pstmt.setString(2, p.getPassword());
+				pstmt.setString(3, p.getTitle());
+				pstmt.setString(4, p.getForename());
+				pstmt.setString(5, p.getSurname());
+				pstmt.setInt(6, p.getPhoneNumber());
+				pstmt.setInt(7, p.getHost());
+				pstmt.setInt(8, p.getGuest());
+				pstmt.setString(9, a.getHouse());
+				pstmt.setString(10, a.getPostCode());
+				
+				pstmt.executeUpdate();
+				
+				if (p.getHost() == 1) {
+					pstmt = con.prepareStatement("INSERT INTO `team023`.`Host` (`userID`, `password`, `superhost`) VALUES (?, ?, ?);");
+					
+					pstmt.setString(1, p.getEmail());
+					pstmt.setString(2, p.getPassword());
+					pstmt.setInt(3, 0);
+					
+					pstmt.executeUpdate();
+				}
+				
+				if (p.getGuest() == 1) {
+					pstmt = con.prepareStatement("INSERT INTO `team023`.`Guest` (`userID`, `password`) VALUES (?, ?);");
+					
+					pstmt.setString(1, p.getEmail());
+					pstmt.setString(2, p.getPassword());
+					
+					pstmt.executeUpdate();
+				}
+			}
+			catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			closeAll(res, stmt, pstmt, con);
+		}
+	}
+	
+	public void addUserAddressInDatabase(Address a, String statement) throws SQLException, Exception {
+		try {
+			con = DriverManager.getConnection(SERVER, DBUSER, PASSWORD);
+			try {
+				pstmt = con.prepareStatement(statement);
+				
+				pstmt.setString(1, a.getHouse());
+				pstmt.setString(2, a.getPostCode());
+				pstmt.setString(3, a.getStreetName());
+				pstmt.setString(4, a.getPlaceName());
+				
+				pstmt.executeUpdate();
+			}
+			catch (SQLException ex) {
+				ex.printStackTrace();
+			}
+		}
+		catch (Exception ex) {
+			ex.printStackTrace();
+		} finally {
+			closeAll(res, stmt, pstmt, con);
+		}
+	}
+	
+	public void closeAll(ResultSet res, Statement stmt, PreparedStatement pstmt, Connection con) {
 		
 		if (res != null) {
 			try {
@@ -95,6 +179,15 @@ public class DatabaseCommunication {
 	            e.printStackTrace();
 	        }
 	    }
+	    if (pstmt != null) {
+	        try {
+	            if (!pstmt.isClosed()) {
+	            	pstmt.close();
+	            }
+	        } catch (SQLException e) {
+	            e.printStackTrace();
+	        }
+	    }
 
 	    if (con != null) {
 	        try {
@@ -106,5 +199,4 @@ public class DatabaseCommunication {
 	        }
 		}
 	}
-	
 }
