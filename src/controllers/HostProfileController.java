6f8cb6df43bfc56c7c3ad7bb13b8a270f6c78349
Andreas-Evripidou <83755870+Andreas-Evripidou@users.noreply.github.com>
2021-11-19 00:01:11 +0000

    Displaying properties lists still need to be done.

--- src/controllers/HostProfileController.java
+++ src/controllers/HostProfileController.java
@@ -14,92 +14,13 @@ import main.Review;
 
 public class HostProfileController {
 	
-	public static ArrayList<Property> getPropertiesByHostID(String hostID) {
-		String query = "SELECT * FROM team023.Property WHERE userID='" + hostID + "'";
-		DatabaseCommunication db = new DatabaseCommunication();
-		ChargeBandController cb = new ChargeBandController();
-		FacilitiesController fc = new FacilitiesController();
+	public Object[][] getRequestColumnData(){
 		
-		String propertyID;
-		String userID;
-		String shortName;
-		String description;
-		String generalLocation;
-		Boolean isBreakfastOffered;
-		int maximumGuest;
-		String house;
-		String postcode;
 		
-		ArrayList<Property> allProperties = new ArrayList<>();
 		
-		try {
-			ResultSet results = db.queryExecute(query);
-			while (results.next()) {
-				propertyID = results.getString(1);
-				userID = results.getString(2);
-				shortName = results.getString(3);
-				description = results.getString(4);
-				generalLocation = results.getString(5);
-				isBreakfastOffered = results.getBoolean(6);
-				maximumGuest = results.getInt(7);
-				house = results.getString(8);
-				postcode = results.getString(9);
-				
-				Address address = getAddress(postcode, house);
-				List<ChargeBand> chargeBands = cb.getChargeBandsByPropertyID(propertyID);
-				Bathing bathingFac = fc.getBathingFacByPropertyID(propertyID);
-				Sleeping sleepingFac = fc.getSleepingFacByPropertyID(propertyID);
-				Kitchen kitchenFac = fc.getKitchenFacByPropertyID(propertyID);
-				Living livingFac = fc.getLivingFacByPropertyID(propertyID);
-				Outdoor outdoorFac = fc.getOutdoorFacByPropertyID(propertyID);
-				Utility utilityFac = fc.getUtilityFacByPropertyID(propertyID);
-				
-				System.out.println("eimai dame");
-				Property property = new Property(chargeBands, address, shortName, description, isBreakfastOffered, bathingFac, kitchenFac, livingFac, outdoorFac, sleepingFac, utilityFac);
-				allProperties.add(property);
-			}
-			
-			return allProperties;
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
-			}
 		return null;
 	}
 	
-	public static Address getAddress(String postcodeCPK, String houseCPK){
-		String query = "SELECT * FROM team023.Address WHERE postcode='" + postcodeCPK + "' AND house='"+ houseCPK +"'";
-		DatabaseCommunication db = new DatabaseCommunication();		
-		
-		String house;
-		String postcode;
-		String streetName;
-		String placeName;
-		
-		
-		Address address = null;
-		try {
-			
-			ResultSet results = db.queryExecute(query);
-			while (results.next()) {
-				
-				house = results.getString(1);
-				postcode = results.getString(2);
-				streetName = results.getString(3);
-				placeName = results.getString(4);
-				
-				address = new Address(house, postcode, placeName, streetName);
-			}
-			
-			
-			return address;
-			} catch (Exception e) {
-				e.printStackTrace();
-			} finally {
-				db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
-			}
-		
-		return null;
-	};
+	
+	
 }
