5ff10215f96c93d31394d1a07333d4b16c73cc6e
Andreas Matoli <amatoli1@sheffield.ac.uk>
2021-11-21 19:45:15 +0000

    added useful methods used in the guest profile page in order to be able to open the viewPropertyPage

--- src/controllers/PersonController.java
+++ src/controllers/PersonController.java
@@ -3,6 +3,8 @@ package controllers;
 import java.sql.ResultSet;
 
 import main.DatabaseCommunication;
+import main.Host;
+import main.Person;
 
 	public class PersonController {
 		public String getNameByUserID(String userID) {
@@ -50,4 +52,91 @@ import main.DatabaseCommunication;
 					}
 				return null;
 			}
+		
+		public String getUserIDByPropertyID(String propertyID) {
+			String query = "SELECT userID FROM team023.Property WHERE propertyID='" + propertyID + "'";
+			DatabaseCommunication db = new DatabaseCommunication();
+			
+			
+			String userID = null;
+			
+			try {
+				ResultSet results = db.queryExecute(query);
+				while (results.next()) {
+					userID = results.getString(1);
+		
+				}
+				
+				return userID;
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
+				}
+			return null;
+		}
+		
+		public Person getPersonByPropertyID(String propertyID) {
+			String userID = getUserIDByPropertyID(propertyID);
+			String query = "SELECT title, name, surname, userID, contactDetails, host, guest FROM team023.Person WHERE userID='" + userID + "'";
+			DatabaseCommunication db = new DatabaseCommunication();
+			
+			
+			Person person = null;
+			
+			try {
+				ResultSet results = db.queryExecute(query);
+				while (results.next()) {
+					String title = results.getString(1);
+					String forename = results.getString(2);
+					String surnname = results.getString(3);
+					String email = results.getString(4);
+					int contactDetails = results.getInt(5);
+					int isHost = results.getInt(6);
+					int isGuest = results.getInt(7);
+					
+					person = new Person(title, forename, surnname, email, contactDetails, isHost, isGuest, null);
+				}
+				
+				return person;
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
+				}
+			return null;
+		}
+		
+		public Host getHostByPropertyID(String propertyID) {
+			Person person = getPersonByPropertyID(propertyID);
+			String query = "SELECT superhost FROM team023.Host WHERE userID='" + person.getEmail() + "'";
+			DatabaseCommunication db = new DatabaseCommunication();
+			
+			
+			Host host = null;
+			
+			try {
+				ResultSet results = db.queryExecute(query);
+				while (results.next()) {
+					boolean superhost = results.getBoolean(1);
+					
+					host = new Host(person.getTitle(), person.getForename(), person.getSurname(), person.getEmail(), person.getPhoneNumber(), person.getHost(), person.getGuest(), null, person.getForename(), superhost);
+				}
+				
+				return host;
+				} catch (Exception e) {
+					e.printStackTrace();
+				} finally {
+					db.closeAll(db.getResultSet(), db.getStatement(), db.getPreparedStatement(), db.getConnection());
+				}
+			return null;
+		}
+		
+		
+//		public static void main(String [] args) {
+//			PersonController pController = new PersonController();
+//			System.out.println(pController.getPersonByPropertyID("33").getEmail());
+//			
+//		}
+		
 }
